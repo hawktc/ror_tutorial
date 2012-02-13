8dcd3e8b0e32d63c5c9ee4a6ddb0faf4ec666088
Tan Chao <tan.chao.hawk@gmail.com>
2012-02-12 20:20:31 -0800

    init tutorial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorTutorial::Application.config.secret_token = '009d27061eb3358421c78d6b230941f7a347f78eea39d6ec0ce81b3590cb3fbc9d726639aa371da2f416da553a3d86938be0916454a9bba40a412a15422a389e'
